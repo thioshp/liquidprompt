2fad6dffebfc2f1dc454a5420a0bac11b50fe474
thioshp <thioshp@gmail.com>
2020-06-03 14:50:10 +0300

    Added mkdir statements, corrected paths and included comments.
    Signed-off-by: thioshp <thioshp@gmail.com>

--- contrib/dist/debian/create_deb.sh
+++ contrib/dist/debian/create_deb.sh
@@ -1,18 +1,32 @@
 #!/bin/sh
 
 echo "Creating Debian's package..."
+
+# Edit the control file
 nano ./liquidprompt/DEBIAN/control
 
+# Create directory structure
+echo "Creating directories..."
+mkdir -p ./liquidprompt/usr/bin
+mkdir ./liquidprompt/etc
+
+# Copy required files
 echo "Copying files..."
-cp ../../liquidprompt ./liquidprompt/usr/bin/liquidprompt
-cp ../../liquidpromptrc-dist ./liquidprompt/etc/liquidpromptrc
+cp ../../../liquidprompt ./liquidprompt/usr/bin/liquidprompt
+cp ../../../liquidpromptrc-dist ./liquidprompt/etc/liquidpromptrc
+
+# Make the liquidprompt script executable
 chmod a+x ./liquidprompt/usr/bin/liquidprompt
 
+# Generate liquidprompt.deb
 echo "Building liquidprompt.deb..."
 dpkg-deb -b liquidprompt
 
+# Remove all copied files
 echo "Deleting files..."
 rm -f ./liquidprompt/etc/*
 rm -f ./liquidprompt/usr/bin/*
 
+# Alert on completing the above tasks
 echo "Done !"
+
